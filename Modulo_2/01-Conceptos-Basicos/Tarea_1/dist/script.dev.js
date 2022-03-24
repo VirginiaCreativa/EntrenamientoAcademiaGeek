52c728b9d3d668a3b03615ad742c98b5a489de30
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-03-24 15:44:00 -0500

    Modulo_2_1 Tarea 1 part 3

--- Modulo_2/01-Conceptos-Basicos/Tarea_1/dist/script.dev.js
+++ Modulo_2/01-Conceptos-Basicos/Tarea_1/dist/script.dev.js
@@ -1,12 +1,14 @@
 "use strict";
 
-var queryCalcNum1 = prompt('Dame número');
-var queryCalcNum2 = prompt('Dame otro número');
+// let queryCalcNum1 = prompt('Dame número');
+// let queryCalcNum2 = prompt('Dame otro número');
 var addNum1 = document.querySelectorAll('.calc_box .calc_num1');
-var addNum2 = document.querySelectorAll('.calc_box .calc_num2');
-addNum1.forEach(function (i) {
-  i.innerHTML = queryCalcNum1 || 0;
-});
-addNum2.forEach(function (i) {
-  i.innerHTML = queryCalcNum2 || 0;
-});
+var addNum2 = document.querySelectorAll('.calc_box .calc_num2'); // addNum1.forEach((i) => {
+//   i.innerHTML = queryCalcNum1 || 0;
+// });
+// addNum2.forEach((i) => {
+//   i.innerHTML = queryCalcNum2 || 0;
+// });
+
+addNum1[0].innerHTML = 2;
+addNum2[0].innerHTML = 43;
