081b082a2bb682cc419b8d4de4ed2a8ddb4c43aa
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-03-24 16:09:18 -0500

    Modulo_2_1 Tarea 1 part 5

--- Modulo_2/01-Conceptos-Basicos/Tarea_1/script.js
+++ Modulo_2/01-Conceptos-Basicos/Tarea_1/script.js
@@ -3,6 +3,10 @@ let queryCalcNum2 = prompt('Dame otro número');
 
 let addNum1 = document.querySelectorAll('.calc_box .calc_num1');
 let addNum2 = document.querySelectorAll('.calc_box .calc_num2');
+let resultSuma = document.querySelector('.calc_result_suma');
+let resultReste = document.querySelector('.calc_result_reste');
+let resultMultiple = document.querySelector('.calc_result_multiple');
+let resultDivision = document.querySelector('.calc_result_division');
 
 addNum1.forEach((i) => {
   i.innerHTML = queryCalcNum1 || 0;
@@ -10,3 +14,8 @@ addNum1.forEach((i) => {
 addNum2.forEach((i) => {
   i.innerHTML = queryCalcNum2 || 0;
 });
+
+resultSuma.innerHTML = Number(queryCalcNum1) + Number(queryCalcNum2);
+resultReste.innerHTML = Number(queryCalcNum1) - Number(queryCalcNum2);
+resultMultiple.innerHTML = Number(queryCalcNum1) * Number(queryCalcNum2);
+resultDivision.innerHTML = Number(queryCalcNum1) / Number(queryCalcNum2);
