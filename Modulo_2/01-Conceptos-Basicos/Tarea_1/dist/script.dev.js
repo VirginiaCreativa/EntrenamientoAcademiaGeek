658519b770f4481f548a46466b6e2d81bc488805
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-03-24 15:59:46 -0500

    Modulo_2_1 Tarea 1 part 4

--- Modulo_2/01-Conceptos-Basicos/Tarea_1/dist/script.dev.js
+++ Modulo_2/01-Conceptos-Basicos/Tarea_1/dist/script.dev.js
@@ -1,14 +1,12 @@
 "use strict";
 
-// let queryCalcNum1 = prompt('Dame número');
-// let queryCalcNum2 = prompt('Dame otro número');
+var queryCalcNum1 = prompt('Dame número');
+var queryCalcNum2 = prompt('Dame otro número');
 var addNum1 = document.querySelectorAll('.calc_box .calc_num1');
-var addNum2 = document.querySelectorAll('.calc_box .calc_num2'); // addNum1.forEach((i) => {
-//   i.innerHTML = queryCalcNum1 || 0;
-// });
-// addNum2.forEach((i) => {
-//   i.innerHTML = queryCalcNum2 || 0;
-// });
-
-addNum1[0].innerHTML = 2;
-addNum2[0].innerHTML = 43;
+var addNum2 = document.querySelectorAll('.calc_box .calc_num2');
+addNum1.forEach(function (i) {
+  i.innerHTML = queryCalcNum1 || 0;
+});
+addNum2.forEach(function (i) {
+  i.innerHTML = queryCalcNum2 || 0;
+});
--- Modulo_2/01-Conceptos-Basicos/Tarea_1/script.js
+++ Modulo_2/01-Conceptos-Basicos/Tarea_1/script.js
@@ -1,15 +1,12 @@
-// let queryCalcNum1 = prompt('Dame número');
-// let queryCalcNum2 = prompt('Dame otro número');
+let queryCalcNum1 = prompt('Dame número');
+let queryCalcNum2 = prompt('Dame otro número');
 
 let addNum1 = document.querySelectorAll('.calc_box .calc_num1');
 let addNum2 = document.querySelectorAll('.calc_box .calc_num2');
 
-// addNum1.forEach((i) => {
-//   i.innerHTML = queryCalcNum1 || 0;
-// });
-// addNum2.forEach((i) => {
-//   i.innerHTML = queryCalcNum2 || 0;
-// });
-
-addNum1[0].innerHTML = 2;
-addNum2[0].innerHTML = 43;
+addNum1.forEach((i) => {
+  i.innerHTML = queryCalcNum1 || 0;
+});
+addNum2.forEach((i) => {
+  i.innerHTML = queryCalcNum2 || 0;
+});
