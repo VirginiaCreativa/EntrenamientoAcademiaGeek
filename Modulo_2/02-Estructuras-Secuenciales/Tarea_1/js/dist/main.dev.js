c1120066818976dd240dd3915660ec09e665b669
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-03-31 11:51:24 -0500

    Modulo_2_2 Tarea_1 part 14

--- Modulo_2/02-Estructuras-Secuenciales/Tarea_1/js/dist/main.dev.js
+++ Modulo_2/02-Estructuras-Secuenciales/Tarea_1/js/dist/main.dev.js
@@ -8,20 +8,26 @@ var numberBoy = 0;
 var localGirlNumber = '.card_inform_woman_number';
 var localBoyNumber = '.card_inform_man_number';
 var localTotalNumber = '.card_inform_total_number';
-var localGirlPorcent = '.card_chart_porcent_woman';
-var localBoylPorcent = '.card_chart_porcent_man';
+var chartGirlPorcent = '.card_chart_porcent_woman';
+var chartBoylPorcent = '.card_chart_porcent_man';
+var resultGirllPorcent = '.card_porcent_woman span';
+var resultBoylPorcent = '.card_porcent_man span';
 var addNumberGirl = document.querySelector(localGirlNumber);
 var addNumberBoy = document.querySelector(localBoyNumber);
 var addNumberTotal = document.querySelector(localTotalNumber);
-var addPorcentGirl = document.querySelector(localGirlPorcent);
-var addPorcentBoy = document.querySelector(localBoylPorcent); // let queryGirlNumber = Number(prompt('Ingrese la cantidad de Chicas?'));
-// let queryBoyNumber = Number(prompt('Ingrese la cantidad de Chicos?'));
-
+var addPorcentGirl = document.querySelector(chartGirlPorcent);
+var addPorcentBoy = document.querySelector(chartBoylPorcent);
+var addResultGirllPorcent = document.querySelector(resultGirllPorcent);
+var addResultBoylPorcent = document.querySelector(resultBoylPorcent);
+var queryGirlNumber = Number(prompt('Ingrese la cantidad de Chicas?'));
+var queryBoyNumber = Number(prompt('Ingrese la cantidad de Chicos?'));
 porcentGirl = queryGirlNumber / (queryGirlNumber + queryBoyNumber) * 100;
 porcentBoy = queryBoyNumber / (queryGirlNumber + queryBoyNumber) * 100;
 addNumberGirl.textContent = queryGirlNumber || numberGirl;
 addNumberBoy.textContent = queryBoyNumber || numberBoy;
-addNumberTotal.textContent = queryGirlNumber + queryBoyNumber; // console.log(queryGirlNumber);
+addNumberTotal.textContent = queryGirlNumber + queryBoyNumber;
+addResultGirllPorcent.textContent = "".concat(porcentGirl.toFixed(1), "%") || numberGirl;
+addResultBoylPorcent.textContent = "".concat(porcentBoy.toFixed(1), "%") || numberBoy; // console.log(queryGirlNumber);
 // console.log(porcentBoy);
 
 if (queryGirlNumber === 0) {
--- Modulo_2/02-Estructuras-Secuenciales/Tarea_1/js/main.js
+++ Modulo_2/02-Estructuras-Secuenciales/Tarea_1/js/main.js
@@ -7,18 +7,23 @@ let numberBoy = 0;
 const localGirlNumber = '.card_inform_woman_number';
 const localBoyNumber = '.card_inform_man_number';
 const localTotalNumber = '.card_inform_total_number';
-const localGirlPorcent = '.card_chart_porcent_woman';
-const localBoylPorcent = '.card_chart_porcent_man';
+const chartGirlPorcent = '.card_chart_porcent_woman';
+const chartBoylPorcent = '.card_chart_porcent_man';
+const resultGirllPorcent = '.card_porcent_woman span';
+const resultBoylPorcent = '.card_porcent_man span';
 
 let addNumberGirl = document.querySelector(localGirlNumber);
 let addNumberBoy = document.querySelector(localBoyNumber);
 let addNumberTotal = document.querySelector(localTotalNumber);
 
-let addPorcentGirl = document.querySelector(localGirlPorcent);
-let addPorcentBoy = document.querySelector(localBoylPorcent);
+let addPorcentGirl = document.querySelector(chartGirlPorcent);
+let addPorcentBoy = document.querySelector(chartBoylPorcent);
 
-// let queryGirlNumber = Number(prompt('Ingrese la cantidad de Chicas?'));
-// let queryBoyNumber = Number(prompt('Ingrese la cantidad de Chicos?'));
+let addResultGirllPorcent = document.querySelector(resultGirllPorcent);
+let addResultBoylPorcent = document.querySelector(resultBoylPorcent);
+
+let queryGirlNumber = Number(prompt('Ingrese la cantidad de Chicas?'));
+let queryBoyNumber = Number(prompt('Ingrese la cantidad de Chicos?'));
 
 porcentGirl = (queryGirlNumber / (queryGirlNumber + queryBoyNumber)) * 100;
 porcentBoy = (queryBoyNumber / (queryGirlNumber + queryBoyNumber)) * 100;
@@ -26,6 +31,9 @@ porcentBoy = (queryBoyNumber / (queryGirlNumber + queryBoyNumber)) * 100;
 addNumberGirl.textContent = queryGirlNumber || numberGirl;
 addNumberBoy.textContent = queryBoyNumber || numberBoy;
 addNumberTotal.textContent = queryGirlNumber + queryBoyNumber;
+
+addResultGirllPorcent.textContent = `${porcentGirl.toFixed(1)}%` || numberGirl;
+addResultBoylPorcent.textContent = `${porcentBoy.toFixed(1)}%` || numberBoy;
 
 // console.log(queryGirlNumber);
 // console.log(porcentBoy);
