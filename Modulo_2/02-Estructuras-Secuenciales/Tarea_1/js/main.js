09ad99ef322de744af2fc71cdcddc15210b526d7
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-03-31 10:41:59 -0500

    Modulo_2_2 Tarea_1 part 9

--- Modulo_2/02-Estructuras-Secuenciales/Tarea_1/js/main.js
+++ Modulo_2/02-Estructuras-Secuenciales/Tarea_1/js/main.js
@@ -2,10 +2,22 @@ let total;
 let numberGirl = 10;
 let numberBoy = 20;
 
-let addNumberGirl = document.querySelector('.card_inform_woman_number');
-let addNumberBoy = document.querySelector('.card_inform_man_number');
-let addNumberTotal = document.querySelector('.card_inform_total_number');
+const localGirlNumber = '.card_inform_woman_number';
+const localBoyNumber = '.card_inform_man_number';
+const localTotalNumber = '.card_inform_total_number';
+const localGirlPorcent = '.card_chart_porcent_woman';
+const localBoylPorcent = '.card_chart_porcent_man';
+
+let addNumberGirl = document.querySelector(localGirlNumber);
+let addNumberBoy = document.querySelector(localBoyNumber);
+let addNumberTotal = document.querySelector(localTotalNumber);
+
+let addPorcentGirl = document.querySelector(localGirlPorcent);
+let addPorcentBoy = document.querySelector(localBoylPorcent);
 
 addNumberGirl.textContent = numberGirl;
 addNumberBoy.textContent = numberBoy;
 addNumberTotal.textContent = numberGirl + numberBoy;
+
+addPorcentGirl.style.width = '80%';
+addPorcentBoy.style.width = '50%';
