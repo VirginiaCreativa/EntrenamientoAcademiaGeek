7fdab714e0294338c5dc36910ef0b82441771327
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-04-05 14:28:17 -0500

    Modulo_2_3 Tarea_3 part 10

--- Modulo_2/03-Estructuras-Condicionales/Tarea_3/js/dist/main.dev.js
+++ Modulo_2/03-Estructuras-Condicionales/Tarea_3/js/dist/main.dev.js
@@ -5,25 +5,31 @@ var descuentoClave_2 = 20;
 var btnDescuento = document.querySelector('.btnDescuento');
 var showResult = document.querySelector('.BoxMarker_Result');
 var inputClave = document.querySelectorAll('input[name="form-radio"]');
-var inputArticulo = document.querySelector('#Input_Articulo');
-var inputPrecio = document.querySelector('#Input_Precio');
+var inputArticulo = document.querySelector('#Input_Articulo').value;
+var inputPrecio = document.querySelector('#Input_Precio').value;
 var addArticulo = document.querySelector('.BoxComprarResult_Articulo');
 var addClave = document.querySelector('.BoxComprarResult_Clave');
 var addPrecio = document.querySelector('.BoxComprarResult_Precio');
 var addDescuento = document.querySelector('.BoxComprarResult_Descuento');
+var isBoxFormsBottom = document.querySelector('.BoxMarker_Forms');
 btnDescuento.addEventListener('click', function (ev) {
   var selectedClave;
 
   for (var i = 0; i < inputClave.length; i++) {
     if (inputClave[i].checked) {
-      console.log(inputClave[i].value);
       selectedClave = inputClave[i].value;
       break;
     }
   }
 
+  var BilleteCOP = new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    minimumFractionDigits: 0
+  }).format(inputPrecio);
+  addArticulo.textContent = inputArticulo;
+  addPrecio.textContent = BilleteCOP;
   addClave.textContent = selectedClave === 'clave_1' ? 'Clave 1' : 'Clave 2';
+  isBoxFormsBottom.style.marginBottom = '60px';
   showResult.style.display = 'block';
 });
-console.log(inputClave);
-console.log(btnDescuento);
--- Modulo_2/03-Estructuras-Condicionales/Tarea_3/js/main.js
+++ Modulo_2/03-Estructuras-Condicionales/Tarea_3/js/main.js
@@ -4,26 +4,33 @@ let descuentoClave_2 = 20;
 let btnDescuento = document.querySelector('.btnDescuento');
 let showResult = document.querySelector('.BoxMarker_Result');
 let inputClave = document.querySelectorAll('input[name="form-radio"]');
-let inputArticulo = document.querySelector('#Input_Articulo');
-let inputPrecio = document.querySelector('#Input_Precio');
+let inputArticulo = document.querySelector('#Input_Articulo').value;
+let inputPrecio = document.querySelector('#Input_Precio').value;
 let addArticulo = document.querySelector('.BoxComprarResult_Articulo');
 let addClave = document.querySelector('.BoxComprarResult_Clave');
 let addPrecio = document.querySelector('.BoxComprarResult_Precio');
 let addDescuento = document.querySelector('.BoxComprarResult_Descuento');
+let isBoxFormsBottom = document.querySelector('.BoxMarker_Forms');
 
 btnDescuento.addEventListener('click', (ev) => {
   let selectedClave;
 
   for (var i = 0; i < inputClave.length; i++) {
     if (inputClave[i].checked) {
-      console.log(inputClave[i].value);
       selectedClave = inputClave[i].value;
       break;
     }
   }
+
+  let BilleteCOP = new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    minimumFractionDigits: 0,
+  }).format(inputPrecio);
+
+  addArticulo.textContent = inputArticulo;
+  addPrecio.textContent = BilleteCOP;
   addClave.textContent = selectedClave === 'clave_1' ? 'Clave 1' : 'Clave 2';
+  isBoxFormsBottom.style.marginBottom = '60px';
   showResult.style.display = 'block';
 });
-
-console.log(inputClave);
-console.log(btnDescuento);
