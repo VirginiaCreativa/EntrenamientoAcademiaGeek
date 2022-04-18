e25d0bc3c920a826fa45bbaacc38813eb9055a2d
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-04-18 17:06:05 -0500

    Modulo_3 Tarea_3 part 9

--- Modulo_3/Tarea_1/js/main.js
+++ Modulo_3/Tarea_1/js/main.js
@@ -1,3 +1,5 @@
 let btnCalcular = document.querySelector('.btnCalcular');
+let inputTotalCuenta = document.querySelector('#Input_TotalCuenta');
+let = document.querySelector('#Input_Propina');
 
 btnCalcular.addEventListener('click', (e) => {});
